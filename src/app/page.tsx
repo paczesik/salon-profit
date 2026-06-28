6be9bd90126daa1ef3fba6340ca8d364ff96da35
paczesik <115451599+paczesik@users.noreply.github.com>
2026-06-28 12:12:40 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,39 +34,4 @@ const HomePage: React.FC = () => {
   );
 };
 
-export default HomePage;'use client';
-import Hero from "@/components/Hero";
-import Testimonials from "@/components/Testimonials";
-import Pricing from "@/components/Pricing/Pricing";
-import FAQ from "@/components/FAQ";
-import Benefits from "@/components/Benefits/Benefits";
-import Container from "@/components/Container";
-import Section from "@/components/Section";
-import Stats from "@/components/Stats";
-import CTA from "@/components/CTA";
-import { useLanguage } from '@/i18n/LanguageContext';
-import { translations } from "@/data/translations";
-
-const HomePage: React.FC = () => {
-  const { language } = useLanguage();
-  const t = translations[language];
-  return (
-    <>
-      <Hero />
-      <Container>
-        <Benefits />
-        <Section id="pricing" title={t.pricing.title} description={t.pricing.description}>
-          <Pricing />
-        </Section>
-        <Section id="testimonials" title={t.testimonials.title} description={t.testimonials.description}>
-          <Testimonials />
-        </Section>
-        <FAQ />
-        <Stats />
-        <CTA />
-      </Container>
-    </>
-  );
-};
-
 export default HomePage;
